632decd2e58c3fbc09cc0dd52f2328658874ccb0
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-03-08 19:42:07 +1100

    parser

---
+++ internal/request/request.go
@@ -0,0 +1,17 @@
+package request
+
+import (
+	"io"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error)
